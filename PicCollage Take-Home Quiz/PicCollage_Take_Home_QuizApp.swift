991ceab091ee46c2886cf2062b68da82bdd6fa92
Loner David <davidyang19995@gmail.com>
2026-03-13 14:43:47 +0800

    Initial Commit

---
+++ PicCollage Take-Home Quiz/PicCollage_Take_Home_QuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  PicCollage_Take_Home_QuizApp.swift
+//  PicCollage Take-Home Quiz
+//
+//  Created by Haruaki on 2026/3/13.
+//
+
+import SwiftUI
+
+@main
+struct PicCollage_Take_Home_QuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
